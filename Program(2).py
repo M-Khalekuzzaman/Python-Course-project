a5439cf51ff06406ab7ee4b7e7d4d2fb64fe18fd
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-20 16:43:47 +0600

    Add files via upload

---
+++ Program(2).py
@@ -0,0 +1,46 @@
+   ##########  [Day2]  ##########
+
+#Multiline comment/docstring :
+'''
+Order of operation
+PEMDAS = 1) () 2)Exponentiation 3) Multiplication 4)Divition 5)Addition 6) Subtraction
+'''
+
+'''
+num1 = input("Enter your first number : ")
+num2 = input("Enter your second number : ")
+
+sum = num1 + num2
+print(sum)
+'''
+'''
+value = None
+value2 = bool(value)
+sum = 10
+totalSum = sum + value2
+print(totalSum)
+'''
+# message = "Hello World"
+# print(message[6 : ])
+
+# message = "Rajshahi"
+# print(message[-5 : ].capitalize())
+
+# message = "Rajshahi University"
+# print(message.swapcase())
+'''
+message = "I Love Python Programming"
+number =  15
+print(message + number)
+'''
+'''
+dept1 = "EEE"
+dept2 = "CSE"
+print(dept1 is not dept2)
+'''
+num1 = 10**8
+print(num1)
+num2 = int(10e8)
+print(num2)
+print(num1 == num2)
+
